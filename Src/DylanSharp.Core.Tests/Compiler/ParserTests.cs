7fb383833792b66673895fbc69105561b1b5dfdc
ajlopez <webmaster@ajlopez.com>
2014-01-10 15:56:12 -0300

    Parser first test

---
+++ Src/DylanSharp.Core.Tests/Compiler/ParserTests.cs
@@ -0,0 +1,25 @@
+﻿namespace DylanSharp.Core.Tests.Compiler
+{
+    using System;
+    using DylanSharp.Core.Compiler;
+    using DylanSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void ParseIntegerExpression()
+        {
+            Parser parser = new Parser("123");
+
+            var result = parser.ParseExpression();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ConstantExpression));
+            Assert.AreEqual(123, ((ConstantExpression)result).Value);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+    }
+}
---
+++ Src/DylanSharp.Core/Compiler/Parser.cs
@@ -0,0 +1,32 @@
+﻿namespace DylanSharp.Core.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using DylanSharp.Core.Expressions;
+
+    public class Parser
+    {
+        private Lexer lexer;
+
+        public Parser(string text)
+        {
+            this.lexer = new Lexer(text);
+        }
+
+        public IExpression ParseExpression()
+        {
+            var token = this.lexer.NextToken();
+
+            if (token == null)
+                return null;
+
+            if (token.Type == TokenType.Integer)
+                return new ConstantExpression(int.Parse(token.Value, CultureInfo.InvariantCulture));
+
+            throw new NotImplementedException();
+        }
+    }
+}
