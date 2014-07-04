899ea8f91802b229476f349057e488f0ebbd09d4
ajlopez <webmaster@ajlopez.com>
2014-07-04 10:32:32 -0300

    Identity Expression

---
+++ Src/DylanSharp.Core.Tests/Expressions/IdentityExpressionTests.cs
@@ -0,0 +1,66 @@
+﻿namespace DylanSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DylanSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class IdentityExpressionTests
+    {
+        [TestMethod]
+        public void IdentityTwoEqualIntegers()
+        {
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression(1), new ConstantExpression(1));
+
+            Assert.AreEqual(true, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void IdentityTwoNonEqualIntegers()
+        {
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression(1), new ConstantExpression(2));
+
+            Assert.AreEqual(false, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void IdentityStringInteger()
+        {
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression("foo"), new ConstantExpression(2));
+
+            Assert.AreEqual(false, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void IdentityIntegerString()
+        {
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression(1), new ConstantExpression("foo"));
+
+            Assert.AreEqual(false, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void IdentityTwoDifferentObjects()
+        {
+            var obj1 = new Context();
+            var obj2 = new Context();
+
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression(obj1), new ConstantExpression(obj2));
+
+            Assert.AreEqual(false, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void IdentitySameObject()
+        {
+            var obj1 = new Context();
+
+            IdentityExpression expr = new IdentityExpression(new ConstantExpression(obj1), new ConstantExpression(obj1));
+
+            Assert.AreEqual(true, expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/DylanSharp.Core/Expressions/IdentityExpression.cs
@@ -0,0 +1,25 @@
+﻿namespace DylanSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class IdentityExpression : BinaryExpression
+    {
+        public IdentityExpression(IExpression left, IExpression right)
+            : base(left, right)
+        {
+        }
+
+        public override object Apply(object leftvalue, object rightvalue)
+        {
+            if (leftvalue is System.ValueType)
+                return leftvalue.Equals(rightvalue);
+            if (rightvalue is System.ValueType)
+                return rightvalue.Equals(leftvalue);
+
+            return leftvalue == rightvalue;
+        }
+    }
+}
