758617d1e8673b52eb8977c434e6b94dceae2e96
ajlopez <webmaster@ajlopez.com>
2014-07-07 16:34:54 -0300

    Define expression first tests

---
+++ Src/DylanSharp.Core.Tests/Expressions/DefineExpressionTests.cs
@@ -0,0 +1,47 @@
+﻿namespace DylanSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DylanSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DefineExpressionTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateDefineExpression()
+        {
+            string name = "foo";
+            IExpression valexpr = new ConstantExpression(42);
+            Context context = new Context();
+
+            DefineExpression expr = new DefineExpression(name, null, valexpr);
+
+            Assert.AreEqual(name, expr.Name);
+            Assert.IsNull(expr.TypeName);
+            Assert.AreEqual(valexpr, expr.Expression);
+
+            var result = expr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(42, result);
+            Assert.AreEqual(42, context.GetValue(name));
+        }
+
+        [TestMethod]
+        public void CreateDefineExpressionWithType()
+        {
+            string name = "foo";
+            IExpression valexpr = new ConstantExpression(42);
+            Context context = new Context();
+
+            DefineExpression expr = new DefineExpression(name, "integer", valexpr);
+
+            Assert.AreEqual(name, expr.Name);
+            Assert.AreEqual("integer", expr.TypeName);
+            Assert.AreEqual(valexpr, expr.Expression);
+        }
+    }
+}
---
+++ Src/DylanSharp.Core/Expressions/DefineExpression.cs
@@ -0,0 +1,34 @@
+﻿namespace DylanSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DefineExpression : IExpression
+    {
+        private string name;
+        private string typename;
+        private IExpression expression;
+
+        public DefineExpression(string name, string typename, IExpression expression)
+        {
+            this.name = name;
+            this.typename = typename;
+            this.expression = expression;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public string TypeName { get { return this.typename; } }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public object Evaluate(Context context)
+        {
+            var value = this.expression.Evaluate(context);
+            context.SetValue(this.name, value);
+            return value;
+        }
+    }
+}
